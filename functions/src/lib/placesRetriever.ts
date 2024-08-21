1c3108fa70d29cbe55ad3063c8016ba667b70dd2
Shree Bhagwat <shree@codeaamy.com>
2024-08-21 23:10:57 +0530

    added text embedding functions

--- functions/src/lib/placesRetriever.ts
+++ functions/src/lib/placesRetriever.ts
@@ -1,11 +1,13 @@
 
 
 import { initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import { FieldValue, getFirestore } from 'firebase-admin/firestore';
 
-import { Activity } from './types';
+import { Activity, Place } from './types';
 import { getProjectId } from './genkit.config';
-import { defineRetriever } from '@genkit-ai/ai/retriever';
+import { defineFirestoreRetriever } from '@genkit-ai/firebase';
+import { textEmbeddingGecko } from '@genkit-ai/vertexai';
+import { embed } from '@genkit-ai/ai/embedder';
 
 
 
@@ -31,19 +33,41 @@ export const getActivitiesForDestination = async (placeId: string) => {
 };
 
 /**
+ * Returns an embedded text using textEmbeddingGecko.
+ */
+export const embedGiveText = async (text: string) => {
+    const embeddedText = await embed({
+        embedder: textEmbeddingGecko,
+        content: text,
+    });
+    return embeddedText;
+}
+
+// Gets all the places from firestore and adds the embeddings to the places collection.
+export const addEmbeddingstoPlacesFirestore = async (embeddingText: string) => {
+    const activities = await firestore.collection('places').get();
+    activities.docs.forEach(async (doc) => {
+        const place = doc.data() as Place;
+        const embeddedText = await embedGiveText(place.knownFor);
+        await firestore.collection('places').doc(doc.id).update({
+            embedding: FieldValue.vector(embeddedText),
+        });
+    });
+}
+/**
  * Retriever for places based on the `knownFor` field using the Genkit retriever for Firestore.
  */
-export const placesRetriever = defineRetriever(
-    { name: 'placesRetriever' },
-    async () => ({ documents: [{ content: [{ text: 'TODO' }] }] }),
-);
+// export const placesRetriever = defineRetriever(
+//     { name: 'placesRetriever' },
+//     async () => ({ documents: [{ content: [{ text: 'TODO' }] }] }),
+// );
 // TODO: 1. Replace the lines above with this:
-// export const placesRetriever = defineFirestoreRetriever({
-//     name: 'placesRetriever',
-//     firestore,
-//     collection: 'places',
-//     contentField: 'knownFor',
-//     vectorField: 'embedding',
-//     embedder: textEmbeddingGecko,
-//     distanceMeasure: 'COSINE',
-// });
+export const placesRetriever = defineFirestoreRetriever({
+    name: 'placesRetriever',
+    firestore,
+    collection: 'places',
+    contentField: 'knownFor',
+    vectorField: 'embedding',
+    embedder: textEmbeddingGecko,
+    distanceMeasure: 'COSINE',
+});
